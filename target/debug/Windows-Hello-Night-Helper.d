28335e33d9a44ecbec2e8a479178785822b9f20f
Elio Wang <eli0wang6@gmail.com>
2025-09-27 19:10:02 +0800

    chore: untrack target/ build outputs

--- target/debug/Windows-Hello-Night-Helper.d
+++
@@ -1,1 +0,0 @@
-D:\work\Windows-Hello-Night-Helper\target\debug\Windows-Hello-Night-Helper.exe: D:\work\Windows-Hello-Night-Helper\src\main.rs
--- target/debug/deps/Windows_Hello_Night_Helper.d
+++
@@ -1,5 +0,0 @@
-D:\work\Windows-Hello-Night-Helper\target\debug\deps\Windows_Hello_Night_Helper.d: src\main.rs
-
-D:\work\Windows-Hello-Night-Helper\target\debug\deps\Windows_Hello_Night_Helper.exe: src\main.rs
-
-src\main.rs:
